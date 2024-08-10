d427fa2cafd9946db261d44fe1dd532e46d1c490
medogo <medogo@litslab.net>
2024-08-10 17:31:14 +0200

    append slash fix

--- aleatek/settings.py
+++ aleatek/settings.py
@@ -193,4 +193,6 @@ DJOSER = {
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'*
+
+APPEND_SLASH = False
